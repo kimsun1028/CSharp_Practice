d35f7a478cbeb9ebe0e2b4da7b20650fec541185
kim tae yang <kimsun1028@naver.com>
2026-04-06 14:39:26 +0900

    .

--- Csharpractice.cs
+++ Csharpractice.cs
@@ -9,23 +9,60 @@ namespace Csharp_practice
 
         public static bool isclose(int a, int b, int x, int y)
         {
-            if((a + 1 == x || a - 1 == x) && (b+1==y || b-1 == y))
-                return true;
-            else
-                return false;
+            return Math.Abs(a - x) + Math.Abs(b - y) == 1;
         }
-        public static List<List<int>> union(List<List<int>> matrix, int x, int y)
-        {
-            bool close = false;
-            foreach(List<int> row in matrix)
-            {
-                int a = row[0];
-                int b = row[1];
-                if (isclose(a, b, x, y))
-                {
-                    
-                }
 
+        public static List<List<List<int>>> merge(List<List<List<int>>> matrix, List<int> indexes, List<int> dot)
+        {
+            List<List<int>> mergedGroup = new List<List<int>>();
+            mergedGroup.Add(dot);
+
+            foreach (int idx in indexes)
+            {
+                mergedGroup.AddRange(matrix[idx]); 
+                matrix[idx].Clear();               
+            }
+
+            // 내용물이 비어있는(Clear된) 리스트들을 한꺼번에 삭제
+            matrix.RemoveAll(group => group.Count == 0);
+
+            // 마지막에 통합된 그룹 추가
+            matrix.Add(mergedGroup);
+
+            return matrix;
+        }
+
+        public static List<List<List<int>>> union(List<List<List<int>>> matrix, int x, int y)
+        {
+            List<int> indexes = new List<int>();
+            bool close = false;
+            for(int i = 0; i < matrix.Count; i++)
+            {
+                List<List<int>> row = matrix[i];
+                bool foundInThisRow = false;
+
+                foreach(List<int> dots in row)
+                {   
+                    int a = dots[0], b = dots[1];
+                    if (isclose(a, b, x, y))
+                    {
+                        indexes.Add(i);
+                        close = true;
+                        break;
+                    }
+                }
+            }
+            if(close){
+                if(indexes.Count == 1){
+                    matrix[indexes[0]].Add(new List<int>{x,y});
+                }
+                else
+                {
+                    matrix = merge(matrix, indexes,new List<int>{x,y});
+                }
+            }
+            else{
+                matrix.Add(new List<List<int>> {new List<int>{x,y}});
             }
 
             return matrix;
